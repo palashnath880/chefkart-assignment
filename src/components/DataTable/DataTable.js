91e42b413a0133388c1654c6b6de0f747ae04359
Palash <palashnath880@gmail.com>
2022-12-11 01:37:36 +0600

    Row Color Changing Added"

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -3,20 +3,25 @@ import React, { useEffect, useState } from 'react';
 const DataTable = () => {
 
     const [users, setUsers] = useState(null);
+    const [selectedRow, setSelectedRow] = useState(0);
+    const [selectedFilter, setSelectedFilter] = useState('');
 
     const ascending = () => {
         const ascendingUser = users.sort((a, b) => (a.first_name > b.first_name ? 1 : -1));
         setUsers([...ascendingUser]);
+        setSelectedFilter('asc');
     }
 
     const descending = () => {
         const descendingUser = users.sort((a, b) => (a.first_name < b.first_name ? 1 : -1));
         setUsers([...descendingUser]);
+        setSelectedFilter('desc');
     }
 
     const resetFilter = () => {
         const descendingUser = users.sort((a, b) => (a.id > b.id ? 1 : -1));
         setUsers([...descendingUser]);
+        setSelectedFilter('');
     }
 
     useEffect(() => {
@@ -33,36 +38,36 @@ const DataTable = () => {
             <div className="flex flex-col h-screen">
                 <div className="flex-grow overflow-x-auto">
                     <table className="relative w-full border">
-                        <thead className='z-50'>
+                        <thead>
                             <tr>
                                 <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">ID</th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100 min-w-max">
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50 min-w-max">
                                     <div className="dropdown dropdown-bottom">
                                         <label tabIndex={0} className="cursor-pointer">First Name</label>
                                         <ul tabIndex={0} className="dropdown-content z-50 shadow mt-6 font-normal menu p-2 shadow bg-base-100 rounded-box min-w-max">
-                                            <li><a onClick={resetFilter}>Unsort</a></li>
-                                            <li><a onClick={ascending}>Sort By ASC</a></li>
-                                            <li><a onClick={descending}>Sort By DESC</a></li>
+                                            <li><a onClick={resetFilter} className={selectedFilter === '' && 'active'}>Unsort</a></li>
+                                            <li><a onClick={ascending} className={selectedFilter === 'asc' && 'active'}>Sort By ASC</a></li>
+                                            <li><a onClick={descending} className={selectedFilter === 'desc' && 'active'}>Sort By DESC</a></li>
                                         </ul>
                                     </div>
                                 </th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100">Last Name</th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100">Email</th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100">Gender</th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100">IP Address</th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100">Airport Code</th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100">Time</th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100">Status</th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100">Mobile</th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100">Area</th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100">Show</th>
-                                <th className="sticky top-0 px-1 py-3 bg-slate-100">Edit</th>
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">Last Name</th>
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">Email</th>
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">Gender</th>
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">IP Address</th>
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">Airport Code</th>
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">Time</th>
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">Status</th>
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">Mobile</th>
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">Area</th>
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">Show</th>
+                                <th className="sticky top-0 px-1 py-3 bg-slate-100 z-50">Edit</th>
                             </tr>
                         </thead>
                         <tbody className="divide-y">
                             {users !== null && users.map((user, index) =>
-                                <tr key={index}>
-                                    <td className="px-1 py-2 text-center sticky left-0 bg-slate-100 xl:bg-transparent">
+                                <tr key={index} onClick={() => setSelectedRow(user.id)} className={selectedRow == user.id && 'bg-violet-500 text-slate-50'}>
+                                    <td className="px-1 py-2 text-center sticky left-0 bg-violet-500 xl:bg-transparent">
                                         {user.id}
                                     </td>
                                     <td className="px-1 py-2 text-center">{user.first_name}</td>
